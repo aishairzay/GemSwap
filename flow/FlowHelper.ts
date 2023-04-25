a8427aaac6be722bb40a722115d3ab0e3e3c78ca
Pratik Patel <prpatel05@gmail.com>
2023-04-25 16:40:42 -0400

    WIP

--- flow/FlowHelper.ts
+++ flow/FlowHelper.ts
@@ -1,14 +1,11 @@
+import axios from "axios";
 import { ec as EC } from "elliptic";
 import { SHA3 } from "sha3";
 import { Account } from "../account/Accounts";
 import { Buffer } from "buffer";
 
-const c2j = require('./CadenceToJson.json')
+const c2j = require("./CadenceToJson.json");
 const fcl = require("@onflow/fcl");
-
-fcl.config({
-    'accessNode.api': 'https://access-testnet.onflow.org'
-})
 
 const ec = new EC("secp256k1");
 
@@ -52,28 +49,32 @@ const authorizationFunction = (
 export class FlowHelper {
     fcl: any;
     account: Account | undefined;
+    network: string;
 
     // If only running scripts, you can send in an empty account
     constructor(account: Account | undefined, network = "testnet") {
+        this.network = network;
         this.fcl = fcl;
         this.account = account;
         const config = this.fcl.config({
             "accessNode.api": `${
-                network === 'testnet' ?
-                    'https://access-testnet.onflow.org'
-                    :
-                    'https://rest-mainnet.onflow.org'
+                network === "testnet"
+                    ? "https://rest-testnet.onflow.org"
+                    : "https://rest-mainnet.onflow.org"
             }`,
-        })
+        });
         config.put("flow.network", network);
 
-        const contractAddresses = c2j.vars[network]
+        const contractAddresses = c2j.vars[network];
         Object.keys(contractAddresses).forEach((key) => {
-            if (key && contractAddresses[key]) {   
-                config.put(key, contractAddresses[key])
-                config.put(`system.contracts.${key.slice(2)}`, contractAddresses[key])
+            if (key && contractAddresses[key]) {
+                config.put(key, contractAddresses[key]);
+                config.put(
+                    `system.contracts.${key.slice(2)}`,
+                    contractAddresses[key]
+                );
             }
-        })
+        });
     }
 
     async runScript(
@@ -107,5 +108,94 @@ export class FlowHelper {
         });
 
         return await this.fcl.tx(response).onceSealed();
+    }
+
+    async multiSigSignTransaction(
+        lastTx: any,
+        transactionCode: string,
+        transactionArgs: any = (arg: any, t: any) => [],
+        shouldSend: boolean = true
+    ): Promise<any> {
+        // Will always use the first key for now
+        const keyId = 0;
+        const authorization = authorizationFunction(
+            this.account?.address,
+            keyId,
+            this.account?.privateKey
+        );
+        let payload;
+        let account1 = await this.fcl.account("d96401bb22d88f81");
+        let account2 = await this.fcl.account("d35471304961eb69");
+        console.log(account1);
+        console.log(account2);
+        if (lastTx) {
+            payload = [
+                this.fcl.transaction(transactionCode),
+                this.fcl.ref(lastTx.refBlock),
+                this.fcl.limit(999),
+                this.fcl.authorizations([account1, authorization]),
+                this.fcl.proposer(account1),
+                this.fcl.payer(account1),
+            ];
+        } else {
+            payload = [
+                this.fcl.transaction(transactionCode),
+                this.fcl.limit(999),
+                this.fcl.authorizations([authorization, account2]),
+                this.fcl.proposer(authorization),
+                this.fcl.payer(authorization),
+            ];
+        }
+        console.log("sdfsdfs");
+        const voucher = await this.fcl.serialize(payload);
+        let nextTx = JSON.parse(voucher);
+        console.log(nextTx);
+        if (shouldSend) {
+            nextTx.payloadSigs = lastTx.envelopeSigs;
+            nextTx.authorizers = lastTx.authorizers.concat(nextTx.authorizers);
+            nextTx.proposalKey = lastTx.proposalKey;
+            console.log(nextTx);
+            return await this.sendRawTransaction(nextTx);
+        } else {
+            return nextTx;
+        }
+    }
+
+    async sendRawTransaction(tx: any): Promise<any> {
+        const body = {
+            script: Buffer.from(tx.cadence).toString("base64"),
+            arguments: tx.arguments.map((item: any) =>
+                Buffer.from(JSON.stringify(item)).toString("base64")
+            ),
+            reference_block_id: tx.refBlock,
+            gas_limit: String(tx.computeLimit),
+            payer: tx.payer,
+            proposal_key: {
+                address: tx.proposalKey.address,
+                key_index: String(tx.proposalKey.keyId),
+                sequence_number: String(tx.proposalKey.sequenceNum),
+            },
+            authorizers: tx.authorizers,
+            payload_signatures: tx.payloadSigs.map((item: any) => {
+                return {
+                    address: item.address,
+                    key_index: String(item.keyId),
+                    signature: Buffer.from(item.sig, "hex").toString("base64"),
+                };
+            }),
+            envelope_signatures: tx.envelopeSigs.map((item: any) => {
+                return {
+                    address: item.address,
+                    key_index: String(item.keyId),
+                    signature: Buffer.from(item.sig, "hex").toString("base64"),
+                };
+            }),
+        };
+        console.log(body);
+        const txResult = await axios.post(
+            `${await this.fcl.config().get("accessNode.api")}/v1/transactions`,
+            body
+        );
+        return await fcl.tx(txResult.data.id).onceSealed();
     }
 }
--- flow/__tests__/FlowHelper-test.js
+++ flow/__tests__/FlowHelper-test.js
@@ -8,17 +8,48 @@ test(`can call script`, async () => {
     expect(scriptResult).toEqual("42");
 });
 
-test(`can call tx`, async () => {
-    if (!process.env.FLOW_ADDRESS) {
+//test(`can call tx`, async () => {
+//    if (!process.env.FLOW_ADDRESS) {
+//        return;
+//    }
+//    const flowHelper = new FlowHelper({
+//        address: process.env.FLOW_ADDRESS,
+//        privateKey: process.env.FLOW_PRIVATE_KEY,
+//        publicKey: process.env.FLOW_PUBLIC_KEY,
+//    });
+//    const result = await flowHelper.startTransaction(
+//        "transaction() { prepare(signer: AuthAccount) { } execute { } }"
+//    );
+//    console.log(result);
+//}, 60000);
+
+test(`can multisig tx`, async () => {
+    if (!process.env.FLOW_ADDRESS || !process.env.FLOW_MULTI_SIG_ADDRESS) {
         return;
     }
-    const flowHelper = new FlowHelper({
+    const tx =
+        "transaction() { prepare(signer1: AuthAccount, signer2: AuthAccount) { } execute { } }";
+    const flowHelper1 = new FlowHelper({
         address: process.env.FLOW_ADDRESS,
         privateKey: process.env.FLOW_PRIVATE_KEY,
         publicKey: process.env.FLOW_PUBLIC_KEY,
     });
-    const result = await flowHelper.startTransaction(
-        "transaction() { prepare(signer: AuthAccount) { } execute { } }"
+    const firstTx = await flowHelper1.multiSigSignTransaction(
+        undefined,
+        tx,
+        [],
+        false
+    );
+    const flowHelper2 = new FlowHelper({
+        address: process.env.FLOW_MULTI_SIG_ADDRESS,
+        privateKey: process.env.FLOW_MULTI_SIG_PRIVATE_KEY,
+        publicKey: process.env.FLOW_MULTI_SIG_PUBLIC_KEY,
+    });
+    const result = await flowHelper2.multiSigSignTransaction(
+        firstTx,
+        tx,
+        [],
+        true
     );
     console.log(result);
 }, 60000);

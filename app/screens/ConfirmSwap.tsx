2350002bcd2efdbde364fe21eb5b7829631dd0ee
Pratik Patel <prpatel05@gmail.com>
2023-04-26 16:24:01 -0400

    Have ConfirmSwap Send TX.

--- app/screens/ConfirmSwap.tsx
+++ app/screens/ConfirmSwap.tsx
@@ -1,17 +1,13 @@
-import {
-  View,
-  ScrollView,
-  Text
-} from "react-native";
+import { View, ScrollView, Text } from "react-native";
 import React, { useEffect, useCallback, useLayoutEffect } from "react";
-import { HeaderBackButton } from '@react-navigation/elements';
+import { HeaderBackButton } from "@react-navigation/elements";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { FlowHelper } from "../../flow/FlowHelper";
 import NFTCollection from "../../components/NFTCollection";
-import { getFlowAccount } from '../utils/getFlowAccount';
-import { styles } from '../utils/styles'
-import { transactions } from '../../flow/CadenceToJson.json';
-import QRCode from 'react-native-qrcode-svg';
+import { getFlowAccount } from "../utils/getFlowAccount";
+import { styles } from "../utils/styles";
+import { transactions } from "../../flow/CadenceToJson.json";
+import QRCode from "react-native-qrcode-svg";
 import axios from "axios";
 
 type Props = {
@@ -20,121 +16,128 @@ type Props = {
 };
 
 export default function ConfirmSwap({ route, navigation }: Props) {
-  const multisigJson = route.params.multisigJson
+    const multisigJson = route.params.multisigJson;
 
-  const [address, setAddress] = React.useState<string>("");
+    const [address, setAddress] = React.useState<string>("");
 
-  const [offeredGems, setOfferedGems] = React.useState<any[]>(
-    multisigJson.arguments[0].value.map((a: any) => {
-      return a.value
-    })
-  );
-  const [requestedGems, setRequestedGems] = React.useState<any[]>(
-    multisigJson.arguments[1].value.map((a: any) => {
-      return a.value
-    })
-  );
+    const [offeredGems, setOfferedGems] = React.useState<any[]>(
+        multisigJson.arguments[0].value.map((a: any) => {
+            return a.value;
+        })
+    );
+    const [requestedGems, setRequestedGems] = React.useState<any[]>(
+        multisigJson.arguments[1].value.map((a: any) => {
+            return a.value;
+        })
+    );
 
-  const offererAddress = multisigJson.authorizers[0]
-  const requestedAddress = multisigJson.authorizers[1]
+    const offererAddress = multisigJson.authorizers[0];
+    const requestedAddress = multisigJson.authorizers[1];
 
-  const handleBackButtonPress = useCallback(() => {
-      // Specify the screen you want to navigate to when the back button is pressed
-      navigation.pop()
-  }, [navigation]);
+    const handleBackButtonPress = useCallback(() => {
+        // Specify the screen you want to navigate to when the back button is pressed
+        navigation.pop();
+    }, [navigation]);
 
-  useEffect(() => {
-      const run = async () => {
-          const acc = await getFlowAccount()
-          setAddress(acc.address)
-      }
-      run()
-  }, [])
+    useEffect(() => {
+        const run = async () => {
+            const acc = await getFlowAccount();
+            setAddress(acc.address);
+        };
+        run();
+    }, []);
 
-  useLayoutEffect(() => {
-      navigation.setOptions({
-          headerLeft: () => (
-              <HeaderBackButton
-                  onPress={handleBackButtonPress}
-                  tintColor="black"
-              />
-          ),
-      });
-  }, [navigation, handleBackButtonPress]);
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerLeft: () => (
+                <HeaderBackButton
+                    onPress={handleBackButtonPress}
+                    tintColor="black"
+                />
+            ),
+        });
+    }, [navigation, handleBackButtonPress]);
 
-  useEffect(() => {
-      const getEvent = async () => {
-          const flowHelper = new FlowHelper(undefined);
-          // TODO Get the event from the blockchain
-      };
-      getEvent();
-  }, []);
+    useEffect(() => {
+        const getEvent = async () => {
+            const flowHelper = new FlowHelper(undefined);
+            // TODO Get the event from the blockchain
+        };
+        getEvent();
+    }, []);
 
-  const insets = useSafeAreaInsets();
+    const insets = useSafeAreaInsets();
 
-  return (
-      <>
-          <View
-              style={[
-                  {
-                      flex: 1,
-                      justifyContent: "center",
-                      alignItems: "center",
-                  },
-              ]}
-          >
-              <ScrollView
-                  style={{
-                      marginTop: 60,
-                      
-                  }}
-                  keyboardShouldPersistTaps='handled'
-              >
-                  <View style={{
-                      paddingTop: insets.top,
-                      paddingBottom: insets.bottom,
-                      paddingLeft: insets.left,
-                      paddingRight: insets.right,
-                  }}>
-                        <NFTCollection label="Their Collection" address={offererAddress} />
-                        <Text>What they give:</Text>
-                        <Text>
-                            {JSON.stringify(offeredGems)}
-                        </Text>
-
-                        <NFTCollection label="My Collection" address={requestedAddress} />
-                        <Text>What they take:</Text>
-                        <Text>
-                            {JSON.stringify(requestedGems)}
-                        </Text>
-                  </View>
-                  <Text
+    return (
+        <>
+            <View
+                style={[
+                    {
+                        flex: 1,
+                        justifyContent: "center",
+                        alignItems: "center",
+                    },
+                ]}
+            >
+                <ScrollView
                     style={{
-                        ...styles.text,
-                        ...styles.clickable,
-                        ...styles.largeText
+                        marginTop: 60,
                     }}
-                    onPress={async () => {
-                        // Build second half of multisig trade
-                        const curAccount = await getFlowAccount();
-                        const flowHelper = new FlowHelper(curAccount);
-                        const multiSigTx = await flowHelper.multiSigSignTransaction(
-                            multisigJson,
-                            transactions.SwapGems,
-                            (arg: any, t: any) => [
-                                arg(offeredGems, t.Array(t.UInt64)),
-                                arg(requestedGems, t.Array(t.UInt64))
-                            ],
-                            [offererAddress, requestedAddress],
-                            false // TODO: Should be true
-                        )
-                        console.log('multiSigTx', JSON.stringify(multiSigTx))
-                    }}
-                  >
-                    Confirm Trade
-                  </Text>
-              </ScrollView>
-          </View>
-      </>
-  );
+                    keyboardShouldPersistTaps="handled"
+                >
+                    <View
+                        style={{
+                            paddingTop: insets.top,
+                            paddingBottom: insets.bottom,
+                            paddingLeft: insets.left,
+                            paddingRight: insets.right,
+                        }}
+                    >
+                        <NFTCollection
+                            label="Their Collection"
+                            address={offererAddress}
+                        />
+                        <Text>What they give:</Text>
+                        <Text>{JSON.stringify(offeredGems)}</Text>
+
+                        <NFTCollection
+                            label="My Collection"
+                            address={requestedAddress}
+                        />
+                        <Text>What they take:</Text>
+                        <Text>{JSON.stringify(requestedGems)}</Text>
+                    </View>
+                    <Text
+                        style={{
+                            ...styles.text,
+                            ...styles.clickable,
+                            ...styles.largeText,
+                        }}
+                        onPress={async () => {
+                            // Build second half of multisig trade
+                            const curAccount = await getFlowAccount();
+                            const flowHelper = new FlowHelper(curAccount);
+                            const multiSigTx =
+                                await flowHelper.multiSigSignTransaction(
+                                    multisigJson,
+                                    transactions.SwapGems,
+                                    (arg: any, t: any) => [
+                                        arg(offeredGems, t.Array(t.UInt64)),
+                                        arg(requestedGems, t.Array(t.UInt64)),
+                                    ],
+                                    [offererAddress, requestedAddress],
+                                    true
+                                );
+                            console.log(
+                                "multiSigTx",
+                                JSON.stringify(multiSigTx)
+                            );
+                        }}
+                    >
+                        Confirm Trade
+                    </Text>
+                </ScrollView>
+            </View>
+        </>
+    );
 }
